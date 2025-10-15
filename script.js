41d8ff8154bd6841b7a3c36c2f3349869def9146
uta7770502 <yutakiyama@gmail.com>
2025-10-15 11:23:50 +0900

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".accordion-header").forEach(header => {
-  header.addEventListener("click", () => {
-    const content = header.nextElementSibling;
-    content.classList.toggle("open");
-  });
-});
